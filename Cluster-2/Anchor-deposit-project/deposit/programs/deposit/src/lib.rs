9833a593c4649004ae7b9ffcd2c0974d8b78ebd1
SoreviR <rob.riveros.es@gmail.com>
2023-02-19 15:38:22 +0100

    Deposit updated

--- Cluster-2/Anchor-deposit-project/deposit/programs/deposit/src/lib.rs
+++ Cluster-2/Anchor-deposit-project/deposit/programs/deposit/src/lib.rs
@@ -1,15 +1,282 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::MetadataAccount,
+    token::{Mint, Token, TokenAccount, Transfer as SplTransfer},
+};
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("BEYZEJUyp6xyV8uRFXY7BN7qe5iz4ssng3VhDGx2ywtX");
 
 #[program]
 pub mod deposit {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        deposit_account.deposit_auth = *ctx.accounts.deposit_auth.key;
+        ctx.accounts.deposit_account.auth_bump = *ctx.bumps.get("pda_auth").unwrap();
+        Ok(())
+    }
+
+    //methods for depositing and withdrawing native tokens
+    pub fn deposit_native(ctx: Context<DepositNative>, amount: u64) -> Result<()> {
+        let deposit_account = &mut ctx.accounts.deposit_account;
+        let deposit_auth = &ctx.accounts.deposit_auth;
+        let sys_program = &ctx.accounts.system_program;
+
+        deposit_account.sol_vault_bump = ctx.bumps.get("sol_vault").copied();
+
+        let cpi_accounts = system_program::Transfer {
+            from: deposit_auth.to_account_info(),
+            to: ctx.accounts.sol_vault.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(sys_program.to_account_info(), cpi_accounts);
+
+        system_program::transfer(cpi, amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw_native(ctx: Context<WithdrawNative>, amount: u64) -> Result<()> {
+        let sys_program = &ctx.accounts.system_program;
+        let deposit_account = &ctx.accounts.deposit_account;
+        let pda_auth = &mut ctx.accounts.pda_auth;
+        let sol_vault = &mut ctx.accounts.sol_vault;
+
+        let cpi_accounts = system_program::Transfer {
+            from: sol_vault.to_account_info(),
+            to: ctx.accounts.deposit_auth.to_account_info(),
+        };
+
+        let seeds = &[
+            b"sol_vault",
+            pda_auth.to_account_info().key.as_ref(),
+            &[deposit_account.sol_vault_bump.unwrap()],
+        ];
+
+        let signer = &[&seeds[..]];
+
+        let cpi = CpiContext::new_with_signer(sys_program.to_account_info(), cpi_accounts, signer);
+
+        system_program::transfer(cpi, amount)?;
+
+        Ok(())
+    }
+
+    //methods for depositing and withdrawing fungible SPL tokens
+    pub fn deposit_spl(ctx: Context<DepositSpl>, amount: u64) -> Result<()> {
+        let cpi_accounts = SplTransfer {
+            from: ctx.accounts.from_token_acct.to_account_info(),
+            to: ctx.accounts.to_token_acct.to_account_info(),
+            authority: ctx.accounts.deposit_auth.to_account_info(),
+        };
+
+        let cpi = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+
+        anchor_spl::token::transfer(cpi, amount)?;
+
+        Ok(())
+    }
+
+    pub fn withdraw_spl(ctx: Context<WithdrawSpl>, amount: u64) -> Result<()> {
+        let deposit_account = &ctx.accounts.deposit_account;
+
+        let cpi_accounts = SplTransfer {
+            from: ctx.accounts.from_token_acct.to_account_info(),
+            to: ctx.accounts.to_token_acct.to_account_info(),
+            authority: ctx.accounts.pda_auth.to_account_info(),
+        };
+
+        let seeds = &[
+            b"auth",
+            deposit_account.to_account_info().key.as_ref(),
+            &[deposit_account.auth_bump],
+        ];
+
+        let signer = &[&seeds[..]];
+
+        let cpi = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            cpi_accounts,
+            signer,
+        );
+
+        anchor_spl::token::transfer(cpi, amount)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = deposit_auth, space = DepositBase::LEN)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DepositNative<'info> {
+    #[account(mut, has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"sol_vault", pda_auth.key().as_ref()], bump)]
+    pub sol_vault: SystemAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawNative<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut, seeds = [b"sol_vault", pda_auth.key().as_ref()], bump = deposit_account.sol_vault_bump.unwrap())]
+    pub sol_vault: SystemAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct DepositSpl<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    #[account(
+        init_if_needed,
+        associated_token::mint = token_mint,
+        payer = deposit_auth,
+        associated_token::authority = pda_auth,
+    )]
+    pub to_token_acct: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub from_token_acct: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawSpl<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    #[account(mut)]
+    pub to_token_acct: Account<'info, TokenAccount>,
+    #[account(mut,
+        associated_token::mint = token_mint,
+        associated_token::authority = pda_auth,
+    )]
+    pub from_token_acct: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CreateLimit<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    #[account(init, seeds = [b"limit", token_mint.key().as_ref(), deposit_account.key().as_ref()], bump, payer = deposit_auth, space = Limit::LEN)]
+    pub limit_account: Account<'info, Limit>,
+    pub token_account: Account<'info, TokenAccount>,
+    pub token_mint: Account<'info, Mint>,
+    #[account(owner = Token::id())]
+    pub ask_token_mint: Account<'info, Mint>,
+    pub system_program: Program<'info, System>,
+}
+
+//----------------------------------------------------------
+
+#[derive(Accounts)]
+pub struct UpdateLimit {}
+
+#[derive(Accounts)]
+pub struct RemoveLimit {}
+
+#[derive(Accounts)]
+pub struct AcceptLimit {}
+
+#[derive(Accounts)]
+pub struct MintftAndCreateMetadata<'info> {
+    #[account(has_one = deposit_auth)]
+    pub deposit_account: Account<'info, DepositBase>,
+    #[account(seeds = [b"auth", deposit_account.key().as_ref()], bump = deposit_account.auth_bump)]
+    /// CHECK: no need to check this.
+    pub pda_auth: UncheckedAccount<'info>,
+    #[account(init, payer = deposit_auth, space = MetadataAccount::LEN)]
+    pub metadata: Account<'info, MetadataAccount>,
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    /// CHECK: add constraints later
+    pub edition: UncheckedAccount<'info>,
+    #[account(mut)]
+    pub deposit_auth: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[account]
+pub struct DepositBase {
+    pub deposit_auth: Pubkey,
+    pub auth_bump: u8,
+    pub sol_vault_bump: Option<u8>,
+}
+
+impl DepositBase {
+    const LEN: usize = 8 + 32 + 1 + 1 + 1;
+}
+
+#[account]
+pub struct Limit {
+    pub asset_holding_pda: Option<Pubkey>,
+    pub asset: Asset,
+    pub ask_price_per_asset: u64,
+    pub ask_asset: Asset,
+    pub ask_asset_pda: Option<Pubkey>,
+}
+
+#[account]
+pub struct Asset {
+    pub asset_type: String,
+    pub asset_metadata: Option<Pubkey>,
+    pub asset_mint: Option<Pubkey>,
+}
+
+const OPTION_PUBKEY_LEN: usize = 1 + 32;
+
+impl Limit {
+    const LEN: usize = OPTION_PUBKEY_LEN * 2 + Asset::LEN * 2 + 8;
+}
+
+impl Asset {
+    const LEN: usize = 32 + OPTION_PUBKEY_LEN * 2;
+}
